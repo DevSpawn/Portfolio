585a7cf205cd5a50c3dbd22af982a2ae59ff8196
td267779 <td267779@falmouth.ac.uk>
2023-10-15 19:36:11 +0100

    Add node_modules to gitignore

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "08ca2304e8be856a"; }
+/******/ 		__webpack_require__.h = function() { return "79d7da4f06ea2ff3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
